0a6d0cc4c9f527fd0bf681073735cf0269c5e270
Ghostified <bransonallan@gmail.com>
2024-12-25 00:42:34 +0300

    sample switch case

--- src/ChapterThree_DecisionMaking/SwitchCase.java
+++ src/ChapterThree_DecisionMaking/SwitchCase.java
@@ -9,31 +9,52 @@ public class SwitchCase {
                 + " 2, for $25000 through $50,000 \n"
                 + "3 ,Up to and and including 25,000 per year";
 
+        String defaultMessage = "Invalid Entry";
         String incomeBracketString = JOptionPane. showInputDialog(message);
+
+        double federalTax = 0;
+        double stateTax = 0;
+        double localTax = 0;
+
 
         int incomeBracket = Integer.parseInt(incomeBracketString);
 
         switch (incomeBracket)
         {
-            case incomeBracket : 1
-                double federalTax = .20;
-                double stateTax = .10;
-                double localTax = .05;
+            case 1:
+                federalTax = .20;
+                stateTax = .10;
+                localTax = .05;
                 break;
-            case incomeBracket : 2
+            case 2 :
                  federalTax = .10;
                 stateTax = .05;
                 localTax = .025;
                 break;
-            case incomeBracket : 3
+            case 3:
                  federalTax = .05;
                  stateTax = .025;
                  localTax = .0125;
                 break;
             default:
-                JOptionPane.showMessageDialog("Invalid Entry");
+                JOptionPane. showMessageDialog(null, defaultMessage);
                 System.exit(0);
         }
+
+        String resultMessage = String.format( "\nFor your income bracket (%d):\n" +
+                                                "Federal tax  %.2f%%\n " +
+                                                "State  tax  %.2f%%\n " +
+                                                "Local tax  %.2f%% \n "
+                                                ,incomeBracket ,
+                                                federalTax * 100,
+                                                stateTax * 100,
+                                                localTax *100
+        );
+
+        //double totalTaxes = (localTax%2f * 100) + (stateTax%2f* 100)  + (federalTax%2f * 100);
+
+        //JOptionPane.showMessageDialog(null, totalTaxes);
+        JOptionPane.showMessageDialog(null, resultMessage);
 
     }
 }
