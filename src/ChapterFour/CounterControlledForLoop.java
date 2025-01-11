777370838b8633e02c73cab0a656ccf1ad5543b8
Ghostified <bransonallan@gmail.com>
2025-01-11 23:29:51 +0300

    for loop

---
+++ src/ChapterFour/CounterControlledForLoop.java
@@ -0,0 +1,21 @@
+package ChapterFour;
+
+import javax.swing.*;
+
+//Take five inputs from a user and sum them up  using a for loop
+
+public class CounterControlledForLoop {
+    public static void main(String[] args) {
+        String numberString;
+        double number, total = 0;
+        final int n = 5;
+
+        for (int i = 0; i < n ; i++ ){
+            numberString = JOptionPane.showInputDialog("Enter a  number:? ");
+            number = Double.parseDouble(numberString);
+            total += number;
+        }
+        JOptionPane.showMessageDialog(null, "The total is : " + total);
+        System.exit(0);
+    } //end main
+}//end class
