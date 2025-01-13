77e9fc77aa60ff65b4f938027591f482eb368d34
Ghostified <bransonallan@gmail.com>
2025-01-13 23:15:10 +0300

    for loop

--- src/ChapterFour/CounterControlledForLoop.java
+++ src/ChapterFour/CounterControlledForLoop.java
@@ -5,17 +5,30 @@ import javax.swing.*;
 //Take five inputs from a user and sum them up  using a for loop
 
 public class CounterControlledForLoop {
-    public static void main(String[] args) {
-        String numberString;
-        double number, total = 0;
-        final int n = 5;
+//    public static void main(String[] args) {
+//        String numberString;
+//        double number, total = 0;
+//        final int n = 5;
+//
+//        for (int i = 0; i < n ; i++ ){
+//            numberString = JOptionPane.showInputDialog("Enter a  number:? ");
+//            number = Double.parseDouble(numberString);
+//            total += number;
+//        }
+//        JOptionPane.showMessageDialog(null, "The total is : " + total);
+//        System.exit(0);
+//    } //end main
+public static void main(String[] args) {
+    String numberString;
+    double number, total = 0;
+    final int n = 5;
 
-        for (int i = 0; i < n ; i++ ){
-            numberString = JOptionPane.showInputDialog("Enter a  number:? ");
-            number = Double.parseDouble(numberString);
-            total += number;
-        }
-        JOptionPane.showMessageDialog(null, "The total is : " + total);
-        System.exit(0);
-    } //end main
+    for (int i = 0; i < n ; i++ ){
+        numberString = JOptionPane.showInputDialog("Enter a  number:? ");
+        number = Double.parseDouble(numberString);
+        total += number;
+    }
+    JOptionPane.showMessageDialog(null, "The total is : " + total);
+    System.exit(0);
+}
 }//end class
