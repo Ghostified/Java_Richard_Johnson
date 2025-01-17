a04c5b536c1180f0fdfde272d379eff21591569b
Ghostified <bransonallan@gmail.com>
2025-01-17 23:47:12 +0300

    Nested Loops'

---
+++ src/ChapterFour/NestedLoops.java
@@ -0,0 +1,16 @@
+package ChapterFour;
+
+//demonstrate Nested Lopps
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            for (int j = 1; j <= i ; j++){
+                if (i % j  == 0 ){
+                    System.out.println(i + " is divisible by "  +  j );
+                }
+            }
+        }
+
+    }
+}
