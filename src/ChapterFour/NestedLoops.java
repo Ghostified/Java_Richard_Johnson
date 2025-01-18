1ea9811569de3a77e21d613f5a274dbf5a3216ba
Ghostified <bransonallan@gmail.com>
2025-01-18 23:48:46 +0300

    for loops

--- src/ChapterFour/NestedLoops.java
+++ src/ChapterFour/NestedLoops.java
@@ -4,6 +4,20 @@ package ChapterFour;
 
 public class NestedLoops {
     public static void main(String[] args) {
+//        for (int i = 0; i < 10; i++){
+//            for (int j = 1; j <= i ; j++){
+//                if (i % j  == 0 ){
+//                    System.out.println(i + " is divisible by "  +  j );
+//                }
+//            }
+//        }
+//        for (int i = 0; i < 10; i++){
+//            for (int j = 1; j <= i ; j++){
+//                if (i % j  == 0 ){
+//                    System.out.println(i + " is divisible by "  +  j );
+//                }
+//            }
+//        }
         for (int i = 0; i < 10; i++){
             for (int j = 1; j <= i ; j++){
                 if (i % j  == 0 ){
