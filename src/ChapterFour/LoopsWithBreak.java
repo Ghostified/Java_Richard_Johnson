b90d6ee90fa05e02c8a7895178abac72b686f7f6
Ghostified <bransonallan@gmail.com>
2025-01-19 23:48:33 +0300

    loops with continue statement

--- src/ChapterFour/LoopsWithBreak.java
+++ src/ChapterFour/LoopsWithBreak.java
@@ -2,16 +2,28 @@ package ChapterFour;
 
 public class LoopsWithBreak {
     public static void main(String[] args) {
+//        for (int i = 1; i <= 10 ; i++){
+//
+//            if (i == 5)
+//                break;
+//
+//                for (int j = 1; j <=  i; j++) {
+//                    if ( i % j == 0 ){
+//                        System.out.println( i + " is divisible by " + j);
+//                    }
+//                }
+//
+//        }
         for (int i = 1; i <= 10 ; i++){
 
             if (i == 5)
-                break;
+                continue;
 
-                for (int j = 1; j <=  i; j++) {
-                    if ( i % j == 0 ){
-                        System.out.println( i + " is divisible by " + j);
-                    }
+            for (int j = 1; j <=  i; j++) {
+                if ( i % j == 0 ){
+                    System.out.println( i + " is divisible by " + j);
                 }
+            }
 
         }
     }
