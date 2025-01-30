42f97107cda6142f1adfee52286618487522af19
Ghostified <bransonallan@gmail.com>
2025-01-30 23:52:26 +0300

    excersice 4 pape 215

---
+++ src/ChapterFour/SumIntegersBreak.java
@@ -0,0 +1,27 @@
+package ChapterFour;
+
+import java.util.Scanner;
+
+public class SumIntegersBreak {
+    public static void main(String[] args) {
+
+        int input = 0;
+        int counter;
+
+        int sum = 0;
+
+        while(input <= 10 ){
+
+
+            switch (input){
+                case 1:
+                    System.out.println("Enter a Number: ");
+                    Scanner scan = new Scanner(System.in);
+                    input = scan.nextInt();
+
+            }
+
+        }
+        System.out.println(sum);
+    }
+}
