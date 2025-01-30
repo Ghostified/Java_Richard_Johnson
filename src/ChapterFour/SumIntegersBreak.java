078b5a7aec69de2c27b033af94d3d9611ecfc924
Ghostified <bransonallan@gmail.com>
2025-01-31 00:05:03 +0300

    running sum with break

--- src/ChapterFour/SumIntegersBreak.java
+++ src/ChapterFour/SumIntegersBreak.java
@@ -10,18 +10,74 @@ public class SumIntegersBreak {
 
         int sum = 0;
 
-        while(input <= 10 ){
+//        while(input <= 10 ){
+//
+//
+//            switch (input){
+//                case 1:
+//                    System.out.println("Enter a Number: ");
+//                    Scanner scan = new Scanner(System.in);
+//                    input = scan.nextInt();
+//
+//            }
+//
+//        }
+//        System.out.println(sum);
+        Scanner scan = new Scanner(System.in);
 
 
-            switch (input){
+        for (counter = 0; counter <= 10 ; counter++){
+
+            switch (counter) {
                 case 1:
-                    System.out.println("Enter a Number: ");
-                    Scanner scan = new Scanner(System.in);
+                    System.out.println("Enter a number: ");
                     input = scan.nextInt();
-
+                    sum = sum + input;
+                    break;
+                case 2:
+                    System.out.println("Enter a number: ");
+                    input = scan.nextInt();
+                    sum = sum + input;
+                    break;
+                case 3:
+                    System.out.println("Enter a number: ");
+                    input = scan.nextInt();
+                    sum = sum + input;
+                    break;
+                case 4:
+                    System.out.println("Enter a number: ");
+                    input = scan.nextInt();
+                    sum = sum + input;
+                    break;
+                case 5:
+                    System.out.println("Enter a number: ");
+                    input = scan.nextInt();
+                    sum = sum + input;
+                    break;
+                case 6:
+                    System.out.println("Enter a number: ");
+                    input = scan.nextInt();
+                    sum = sum + input;
+                    break;
+                case 7:
+                    System.out.println("Enter a number: ");
+                    input = scan.nextInt();
+                    sum = sum + input;
+                    break;
+                case 8:
+                    System.out.println("Enter a number: ");
+                    input = scan.nextInt();
+                    sum = sum + input;
+                    break;
+                case 9:
+                    System.out.println("Enter a number: ");
+                    input = scan.nextInt();
+                    sum = sum + input;
+                    break;
+                case 10:
             }
 
+            System.out.println("The total sum:" + sum);
         }
-        System.out.println(sum);
     }
 }
