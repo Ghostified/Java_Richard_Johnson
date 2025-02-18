555192d9bbfeaf87ef433946cc7e5cd4e7debaea
Ghostified <bransonallan@gmail.com>
2025-02-18 23:25:03 +0300

    methods

---
+++ src/ChapterFive/MathMethods.java
@@ -0,0 +1,9 @@
+package ChapterFive;
+
+public class MathMethods {
+    public static void main(String [] args){
+        double number = -3, abssoluteValue;
+        abssoluteValue = Math.abs(number);
+        System.out.println(abssoluteValue);
+    }
+}
