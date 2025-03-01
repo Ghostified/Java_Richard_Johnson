7fcb3d1d25e78acf95aaaca33f4913981295c75a
Ghostified <bransonallan@gmail.com>
2025-03-02 01:13:42 +0300

    sorting part of an array usess the indices of an array and not its index

---
+++ src/ChapterFive/ArrayClass.java
@@ -0,0 +1,28 @@
+package ChapterFive;
+
+import java.util.Arrays;
+
+public class ArrayClass {
+    //Array class contains static methods taht are used by arrays
+    //eg : search, sort , compare , filling and return string representation of arrays
+
+    //sort method sorts an array
+    public static void main(String[] args) {
+        int [] numbers= { 71,26, 302 ,3,64,5,65, 78,99,};
+        //Arrays.sort(numbers);
+        //printArray(numbers);
+
+
+        //sorting part of an array
+        Arrays.sort(numbers, 3,5);
+        printArray(numbers);
+
+    }
+
+     public static void printArray( int [] numbers){
+        for (int i = 0; i < numbers.length; i++ ){
+            System.out.println(numbers [i]);
+        }
+    }
+
+}
