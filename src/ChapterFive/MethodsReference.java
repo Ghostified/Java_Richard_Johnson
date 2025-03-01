b2e37d8aeb7250fe3131f7d1cc93adad03b13b2c
Ghostified <bransonallan@gmail.com>
2025-03-02 00:38:59 +0300

    returning arrays from methods

---
+++ src/ChapterFive/MethodsReference.java
@@ -0,0 +1,33 @@
+package ChapterFive;
+
+import java.awt.*;
+
+public class MethodsReference {
+    public static void main(String[] args) {
+        //an object is destroyed when it is no longer referenced by a variable
+        Point p = getpoint();
+        //System.out.println(p);
+
+        //return
+       int [] numbers = getNumbers();
+       printArrays(numbers);
+
+    }
+
+    public  static Point getpoint(){
+        return  new Point(1,2);
+    }
+
+    //returning arrays from methods
+    public  static int [] getNumbers(){
+        int [] numbers = {1,2,3,4,5,6};
+        return numbers;
+    }
+
+    public static void printArrays (int [] array){
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array [i] + " ");
+        }
+
+    }
+}
