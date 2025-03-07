3dd4ed068edc1ff9b7145d2938eb93c70dbe4280
Ghostified <bransonallan@gmail.com>
2025-03-07 22:28:24 +0300

    method overloading

--- src/ChapterFive/MethodOverloading.java
+++ src/ChapterFive/MethodOverloading.java
@@ -5,8 +5,13 @@ public class MethodOverloading {
 
         //writting the same method with diffeent parameters
         //the method sum takes two parameters in the first method and three in the second
+        // overloaded methods must have diffrent parameters and can have diffrent return types
         System.out.println(sum(2 , 3)); //5
         System.out.println(sum(2,3,4)); //9
+
+        printSomething();
+        printSomething("Welcome to Hogwarts");
+
     }
 
     public  static int sum (int x, int y){
@@ -14,5 +19,13 @@ public class MethodOverloading {
     }
     public static int sum(int y, int x, int z){
         return  x + y + z;
+    }
+
+    public static void printSomething (){
+        System.out.println("Hello Ghost");
+    }
+
+    public static  void printSomething (String fooBar){
+        System.out.println( "Hello " + fooBar);
     }
 }
