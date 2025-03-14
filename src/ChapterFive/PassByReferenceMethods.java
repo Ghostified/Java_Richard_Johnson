0cc934d5796cf53f505152ad0770a6e336dd66e0
Ghostified <bransonallan@gmail.com>
2025-03-14 23:44:38 +0300

    pass by reference is when a method call variaable that are not primitive

---
+++ src/ChapterFive/PassByReferenceMethods.java
@@ -0,0 +1,10 @@
+package ChapterFive;
+
+import java.util.GregorianCalendar;
+
+public class PassByReferenceMethods {
+
+    //Create a date, formart and print
+    GregorianCalendar dateInMain = new GregorianCalendar(2025, 4, 14);
+    long dateInMS = dateInMain.getTimeInMillis();
+}
