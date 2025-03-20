93b310b59e135f1ed085831510d724d1d693bd90
Ghostified <bransonallan@gmail.com>
2025-03-20 23:57:48 +0300

    music world

--- src/ChapterFive/MusicWorldApp.java
+++ src/ChapterFive/MusicWorldApp.java
@@ -1,7 +1,17 @@
 package ChapterFive;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class MusicWorldApp {
+
+    NumberFormat currencyFormatter;
+    NumberFormat percentFormatter;
+    DecimalFormat decimalFormatter;
     public static void main(String[] args) {
+
+    }
+    public  static  void displayOrderOutput (){
 
     }
 }
