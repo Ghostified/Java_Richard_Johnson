faadd70d9acc67b634ae4e35e011529332386422
Ghostified <bransonallan@gmail.com>
2025-04-20 23:55:46 +0300

    easter

--- src/Arrays/Intro.java
+++ src/Arrays/Intro.java
@@ -12,12 +12,30 @@ public class Intro {
 
         double [ ] numbersTwo = {3.5,4.5,5.5, 6.7};
 
-        for (int i = 0; i < numbersTwo.length; i++){
-            System.out.println(numbersTwo[i] + " ");
-        }
+//        for (int i = 0; i < numbersTwo.length; i++){
+//            System.out.println(numbersTwo[i] + " ");
+//        }
+
+        //int [] numbersThree = {1,2,3,4,5};
+        printArray(new Integer[] {1,2,3,4,5});
+
+        //Arrays are passed by reference
+        Integer [] number  = { 0, 1};
+        change(number);
+        printArray(number);
+
 
 
     }
+    public  static  <T> void printArray( T Array []){
+        for (int i = 0; i < Array.length; i++){
+            System.out.println(Array[i]);
+        }
+    }
 
+    public static void change (Integer [] Arrays){
+        Arrays[0] = 1;
+        Arrays[1] = 0;
+    }
 
 }
