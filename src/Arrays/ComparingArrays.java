b686aa4646bf042b1d21787410feaf15cd9df7b5
Ghostified <bransonallan@gmail.com>
2025-04-21 17:24:26 +0300

    .equals method compares the object , while == compares the adresses of the object

--- src/Arrays/ComparingArrays.java
+++ src/Arrays/ComparingArrays.java
@@ -13,11 +13,24 @@ public class ComparingArrays {
         String [] string1 = {"abc","def","ghi"};
         String [] string2= {"abc","def","ghi"};
 
+        //comparing objects
+        String str1 = new String("hello");
+        String str2 = new String("hello");
+
         //using the equals() method;
         System.out.println(Arrays.equals(numbers1,numbers2));//returns false
         System.out.println(Arrays.equals(numbers1,numbers3)); //returns false
         System.out.println(Arrays.equals(string1,string2)); //returns true
         System.out.println(Arrays.equals(string1,1,2,string2,1,2)); //returns true
+
+        //comparing objects
+        System.out.println(str1 == str2); //returns false becasue its compares the adress
+        System.out.println(str1.equals(str2)); //returns true
+        if (str2.equals(str1)){
+            System.out.println("The objects are  equal to each other");
+        } else {
+            System.out.println("The  objects are not equal");
+        }
 
 
     }
