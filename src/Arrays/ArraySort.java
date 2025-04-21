20aac7fdcbd42c4f95a57404d9f4d081e19f34ad
Ghostified <bransonallan@gmail.com>
2025-04-21 16:56:46 +0300

    search an element in an array

--- src/Arrays/ArraySort.java
+++ src/Arrays/ArraySort.java
@@ -7,12 +7,33 @@ public class ArraySort {
         Arrays.sort(nums);
 
 
-        Character [] characters = {'f','g','a','r','b','z'};
+        Character [] characters = { 'A','Z','G','f','g','a','r','b','z'};
         Arrays.sort(characters);
 
         //print output
         printArray(nums);
         printArray(characters);
+
+        //sort a part of the array (from index, to index -1 )
+        Integer []  numbers = {34, 45,67,78,89,45,23,3,2,11,0,-1};
+        Arrays.sort(numbers, 5,12);
+        printArray(numbers);
+
+        //sorting strings
+        String [] string = {"hello", "griffindor", "welcome", "to", "hogwarts"};
+        Arrays.sort(string);
+        printArray(string);
+
+        //search method, first the array must be sorted
+        //return value ---> returns index of the element if exists
+        Integer key1 = 4;
+        Integer myIndex = Arrays.binarySearch(nums,key1);
+        if ( myIndex >= 0){
+            System.out.println("Element found at index: " + myIndex);
+
+        } else {
+            System.out.println("element not found");
+        }
 
     }
 
