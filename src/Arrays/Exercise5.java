33d94ebe028231dcda141ae8c1f20e725a5a49d4
Ghostified <bransonallan@gmail.com>
2025-05-30 23:44:18 +0300

    dev

--- src/Arrays/Exercise5.java
+++ src/Arrays/Exercise5.java
@@ -64,6 +64,16 @@ public class Exercise5 {
 
             }
 
+//                 else {
+//                int temp = array[left];
+//                array[left] = array[right];
+//                array[right] = temp;
+//
+//                left++;
+//                right--;
+//
+//            }
+
 
 
 
