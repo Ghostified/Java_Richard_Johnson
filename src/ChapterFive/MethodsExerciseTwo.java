bfbe9b0377d721e91f157598e8382abea08a8688
Ghostified <bransonallan@gmail.com>
2025-07-18 23:14:29 +0300

    dev

--- src/ChapterFive/MethodsExerciseTwo.java
+++ src/ChapterFive/MethodsExerciseTwo.java
@@ -31,14 +31,6 @@ public class MethodsExerciseTwo {
     }
     //method to find a prime
 
-    public  static  boolean isPrime(int n ){
-        for ( int i = 2; i <= n/2; i++){
-            if (n % i == 0){
-                return false;
-            }
-        }
-        return true;
-    }
 
 
 
