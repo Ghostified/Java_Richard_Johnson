159bba3ed8bd95cb6f0cced220276703532c1125
Ghostified <bransonallan@gmail.com>
2025-10-03 23:53:20 +0300

    dev'

--- src/LeetCode/TwoSumSolution.java
+++ src/LeetCode/TwoSumSolution.java
@@ -5,19 +5,26 @@ import java.util.Map;
 
 public class TwoSumSolution {
     public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+        int [] nums = {2,7, 22, 59};
+        int target = 9;
+        int [] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0]);
 
-        Map<Integer, Integer> Keymap = new HashMap<>();
-
-        for (int = 0; i < nums.length; i++){
-            int complement = target - nums[i];
-
-            if (keymap.containsKeyt (complement)){
-                reurn new int[]{keymap.get(complement), i}
-            }
-
-            keymap.put(nums[i], i)
         }
 
+        public static class TwoSum {
+        public int [] twoSum(int [] nums, int target) {
+            Map <Integer, Integer> numMap = new HashMap<>();
+            for (int i = 0; i < nums.length; i++){
+                int complement = target - nums[i];
+                if (numMap.containsKey(complement)){
+                    return  new int [] {numMap.get(complement), i};
+                }
+                numMap.put(nums[i], i);
+            }
+            return new int[] {};
+        }
     }
 
 }
