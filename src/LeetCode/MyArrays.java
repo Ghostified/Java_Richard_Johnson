b546bdb036629f3f6e970fe99ecb43e451984c79
Ghostified <bransonallan@gmail.com>
2025-10-07 23:57:39 +0300

    c style array declaration

--- src/LeetCode/MyArrays.java
+++ src/LeetCode/MyArrays.java
@@ -2,7 +2,7 @@ package LeetCode;
 
 public class MyArrays {
     public static void main(String[] args) {
-        int array []= {-1,2,3,4,5,6,7,8,9};
+        int[] array = {-1,2,3,4,5,6,7,8,9};
         String[] names = {"James", "Bond", "The"};
         int length = names.length;
 
